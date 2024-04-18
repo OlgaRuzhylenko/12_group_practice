a59e31c775ee824ea15874cc7fddb318c14fa279
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-04-18 12:59:24 +0300

    search on form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import ArticleList from "../components/ArticleList/ArticleList.jsx";
+import SearchForm from "../components/SearchForm/SearchForm.jsx";
 import { useState, useEffect } from "react";
 import { fetchArticles } from "../articles-api.js";
 import css from "./App.module.css";
@@ -8,24 +9,36 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    async function getArticles() {
-      try {
-        setIsLoading(true);
-        const data = await fetchArticles("react");
-        setArticles(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setIsLoading(false);
-      }
+  // useEffect(() => {
+  //   async function getArticles() {
+  //     try {
+  //       setIsLoading(true);
+  //       const data = await fetchArticles("react");
+  //       setArticles(data);
+  //     } catch (error) {
+  //       setError(true);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   getArticles();
+  // }, []);
+  const handleSearch = async (newQuery) => {
+    try {
+      setIsLoading(true);
+      const data = await fetchArticles(newQuery);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
     }
-    getArticles();
-  }, []);
+  };
 
   return (
     <div className={css.container}>
       <h1>HTTP requests in React</h1>
+      <SearchForm onSearch={handleSearch} />
       {error && <b>Oops, error...</b>}
       {isLoading && <b>Please, loading articles...</b>}
       {articles.length > 0 && <ArticleList items={articles} />}
